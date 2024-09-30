510d661541db1dac2a16e81d3ef47d761765c9fc
Matt Krozel <mattkrozel@Kaylis-MacBook-Pro.local>
2024-09-30 14:48:10 -0500

    task 3, add topics and add question now working

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,1 +1,36 @@
 // Define your server actions here
+'use server';
+
+import { revalidatePath } from "next/cache";
+import { insertTopic } from "./data";
+import { redirect } from "next/navigation";
+import { insertQuestion } from "./data";
+
+export async function addTopic(data: FormData) {
+    let topic;
+    try {
+      topic = await insertTopic({
+        title: data.get("title") as string,
+      });
+    } catch (error) {
+      console.error("Database Error:", error);
+      throw new Error("Failed to add topic.");
+    } finally {
+      revalidatePath("/ui/topics/[id]", "page");
+      topic && redirect(`/ui/topics/${topic.id}`);
+    }
+}
+
+export async function addQuestion(question: FormData) {
+    try {
+      insertQuestion({
+        title: question.get("title") as string,
+        topic_id: question.get("topic_id") as string,
+        votes: 0,
+      });
+      revalidatePath("/ui/topics/[id]", "page");
+    } catch (error) {
+      console.error("Database Error:", error);
+      throw new Error("Failed to add question.");
+    }
+}
