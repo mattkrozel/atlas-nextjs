25d437b89ebc2b9ed7039faaeac849c21b6970ae
Matt Krozel <mattkrozel@Kaylis-MacBook-Pro.local>
2024-09-30 14:08:29 -0500

    task 3, topics are navigtable in navbar, questions for topics reflect on specific topic pages

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,6 +1,32 @@
+import { AskQuestion } from "@/components/AskQuestion";
+import { Question } from "@/components/Question";
+import { fetchQuestions, fetchTopic } from "@/lib/data";
+import { HashtagIcon } from "@heroicons/react/24/outline";
+
 export default async function Page({ params }: {params: { id: string } }) {
     //simulates page loading
-    await new Promise((r) => setTimeout(r, 3000));
+    //await new Promise((r) => setTimeout(r, 3000));
+    const topic = await fetchTopic(params.id);
+    const questions = await fetchQuestions(params.id);
 
-    return <div>Topic Page: {params.id}</div>;
+    if (!topic) {
+        return <div>Topic not found</div>;
+    }
+
+    return (
+        <div>
+            <h1 className="text-3xl font-black flex items-center">
+                <HashtagIcon className="h-6 w-6 mr-2" /> {topic.title}
+            </h1>
+            <AskQuestion topic={topic.id} />
+            {questions.map((question) => (
+                <Question
+                    key={question.id}
+                    id={question.id}
+                    text={question.title}
+                    votes={question.votes}
+                />
+            ))}
+        </div>
+    );
 }
